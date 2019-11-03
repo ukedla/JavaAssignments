c00a230bf41937d504b6b55fa2956fc112f8ca23
Uma Kedla <umaudupa@gmail.com>
2019-11-03 10:27:19 -0700

    Committing my Driver program

---
+++ JavaAssignments/src/EmpAssignment/DriverProgram.java
@@ -0,0 +1,27 @@
+package EmpAssignment;
+
+public class DriverProgram {
+
+	public static void main(String[] args) {
+		Employee[] emp = new Employee[3];
+		
+		emp[0] = {"222-31-4444",
+				{"John","s","Doe"},
+				{"3rd Street",{"Phoenix","Arizona","85024"}},
+				45,65000,["Robin","Emma","Valarie"]}; 
+		
+	
+
+	}
+
+	public static void setEmployeeValues(Employee person)
+	{
+		
+	}
+	
+	public static void displayEmployee(Employee person)
+	{
+		
+	}
+
+}
--- JavaAssignments/src/EmpAssignment/Employee.java
+++ JavaAssignments/src/EmpAssignment/Employee.java
@@ -1,12 +1,12 @@
 package EmpAssignment;
 
 public class Employee {
-	String SSN;
+	String sSN;
 	Name empName;
 	Address empAddress;
-	int age;
-	int salary;
-	String[] Dependents;
+	int empAge;
+	int empSalary;
+	String[] dependents;
 	
 
 	public class Name{
@@ -26,9 +26,91 @@ public class Employee {
 		String zip;
 	}
 	
-	public static void main(String[] args) {
-		Employee[] emp = new Employee[10];
+	
+	public Employee() {
 		
 	}
+	
+	public Employee(String ssn, Name name, Address address, int age, int salary, String[] dependents) {
+		this.sSN = ssn;
+		this.empName = name;
+		this.empAddress = address;
+		this.empAge = age;
+		this.empSalary = salary;
+		this.dependents = dependents;
+	}
+	
+	
+	public String getsSN() {
+		return sSN;
+	}
+
+
+
+	public void setsSN(String sSN) {
+		this.sSN = sSN;
+	}
+
+
+
+	public Name getEmpName() {
+		return empName;
+	}
+
+
+
+	public void setEmpName(Name empName) {
+		this.empName = empName;
+	}
+
+
+
+	public Address getEmpAddress() {
+		return empAddress;
+	}
+
+
+
+	public void setEmpAddress(Address empAddress) {
+		this.empAddress = empAddress;
+	}
+
+
+
+	public int getAge() {
+		return empAge;
+	}
+
+
+
+	public void setAge(int age) {
+		this.empAge = age;
+	}
+
+
+
+	public int getSalary() {
+		return empSalary;
+	}
+
+
+
+	public void setSalary(int salary) {
+		this.empSalary = salary;
+	}
+
+
+
+	public String[] getdependents() {
+		return dependents;
+	}
+
+
+
+	public void setDependents(String[] dependents) {
+		dependents = dependents;
+	}
+
+
 
 }
